42241145ed43b988d506e7c5a1bd8bea538061f4
Gurpreet-Moudgill <modgillgaurav6453@gmail.com>
2021-12-20 04:53:17 +0300

    active menu

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile_view');
 const links = document.querySelector('.nav_menu');
+const navlogo = document.querySelector('#nav_logo');
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
@@ -7,3 +8,19 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+// Show active menu while scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#homepage')
+    const aboutMenu = document.querySelector('#about-page')
+    const serviceMenu = document.querySelector('#service-page')
+    let scrollPos = window.scrollY
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }
+
+}
